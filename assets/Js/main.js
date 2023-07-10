067799a86a8c85b6ca8e87e04ff25a3ad9bac672
Sergiobaz <bazan.sergio2399@gmail.com>
2023-07-09 22:00:44 -0300

    segundo commit

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -16,32 +16,129 @@ async function getProducts() {
 function printProducts(db) {
     const productsHTML = document.querySelector('.products')
 
-    console.log(productsHTML);
-
     let html = ``
 
     for (const product of db.Products) {
 
         html += `
         <div class='product'>
-            <p> Id: ${product.id}</p>
-            <p>${product.name}</p>
+
+            <div class="product__img">
+                <img src="${product.image}" alt="image" />
+            </div>
+
+            <div class="product__info">
+                <p>${product.name} | <span><b>Stock</b>: ${product.quantity} </span> </p>
+            <h4>
+                $${product.price}
+                <i class='bx bx-plus' id='${product.id}'></i>
+            </h4>
+
+            </div>
+
+            
         </div>
         `
     }
-    console.log(html);
 
     productsHTML.innerHTML = html
+}
+
+function handleShowCart () {
+    const iconcartHTML = document.querySelector('.bx-cart')
+    const cartHTML = document.querySelector('.cart')
+
+    
+    iconcartHTML.addEventListener('click', function () {
+        cartHTML.classList.toggle("cart__show")
+    })
+}
+
+function addToCartFromProducts(db) {
+    const productsHTML = document.querySelector('.products')
+
+    productsHTML.addEventListener('click', function (e) {
+        if (e.target.classList.contains(`bx-plus`)) {
+            const id = Number(e.target.id);
+
+            const productFind = db.Products.find(
+                product => product.id ===id
+                )
+
+                if (db.cart[productFind.id]) {
+                    if(productFind.quantity === db.cart[productFind.id].amount) return alert('No tenemos más')
+                    db.cart[productFind.id].amount++
+                } else {
+                    db.cart[productFind.id] = {...productFind, amount: 1 }
+                }
+
+                window.localStorage.setItem("cart", JSON.stringify(db.cart))
+
+                printProductsInCart(db)
+        }
+    })
+}
+
+function printProductsInCart(db) {
+    const cardProducts = document.querySelector(".card__products")
+
+let html = ''
+
+for (const product in db.cart) {
+   const { quantity,price,name,image,id,amount} = db.cart[product]
+    html += `
+        <div class="card__product">
+            <div class="card__product--img">
+                <img src="${image}" alt="imagen"/>
+            </div>
+
+            
+            <div class="card__product--body">
+                <h4>${name} | $${price} </h4>
+                <p>Stock: ${quantity}</p>
+            
+                <div class="card__product--body--op">
+                <i class='bx bx-minus'></i>
+                <span> ${amount} unit </span>
+                <i class='bx bx-plus' ></i>
+                <i class='bx bx-trash' ></i>
+                </div>
+
+            </div>
+        </div>
+    `
+    console.log(db.cart[product].image);
+    
+}
+cardProducts.innerHTML = html
 }
 
 async function main() {
 
     const db = {
         Products: (JSON.parse(window.localStorage.getItem('products'))) || await getProducts(),
-        cart: {}
+        cart: JSON.parse(window.localStorage.getItem('cart')) || {}
     }
 
     printProducts(db)
+    handleShowCart ()
+    addToCartFromProducts(db)
+    printProductsInCart(db)
+
+    const cartProdcts = document.querySelector(".card__products")
+
+    cartProdcts.addEventListener("click", function (e) {
+        if (e.target.classList.contains("bx-plus")) {
+            console.log('quieres sumar');
+        }
+        if (e.target.classList.contains("bx-minus")) {
+            console.log('quieres restar');
+        }
+        if (e.target.classList.contains("bx-trash")) {
+            console.log('quieres borrar');
+        }
+    })
+
 }
 
 main()
